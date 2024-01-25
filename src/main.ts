1f40bb7f9b14c0de2e44dd2aacdf07a2a8a4669b
unknown <moatazabdallah952@gmail.com>
2024-01-24 16:04:27 -0800

    first version

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,58 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+  const jobs = [
+    {
+      title: "Software Engineer",
+      image: "images/software-engineer.svg",
+      details:
+        "Responsible for designing, developing and maintaining software systems and applications.",
+      openPositions: "2",
+      link: "#",
+    },
+  
+    {
+      title: "Data Scientist",
+      image: "images/data-scientist.svg",
+      details:
+        "Responsible for collecting, analyzing and interpreting large data sets to help organizations make better decisions.",
+      openPositions: "3",
+      link: "#",
+    },
+  
+    {
+      title: "Project Manager",
+      image: "images/project-manager.svg",
+      details:
+        "Responsible for planning, executing and closing projects on time and within budget.",
+      openPositions: "1",
+      link: "#",
+    },
+  
+    {
+      title: "Product Manager",
+      image: "images/product-manager.svg",
+      details:
+        "Responsible for managing the entire product life cycle, from ideation to launch and post-launch maintenance.",
+      openPositions: "1",
+      link: "#",
+    },
+  
+    {
+      title: "Sales Representative",
+      image: "images/sales-representative.svg",
+      details:
+        "Responsible for reaching out to potential customers and closing sales deals.",
+      openPositions: "4",
+      link: "#",
+    },
+  
+    {
+      title: "Marketing Manager",
+      image: "images/marketing-manager.svg",
+      details:
+        "Responsible for creating and executing marketing strategies to promote a company or product.",
+      openPositions: "1",
+      link: "#",
+    },
+  ];
